9d9fe82d7608a8422acaa7cdcf79d0d4f025538d
Bharat Naik <bharathegde2002@gmail.com>
2026-05-01 23:39:35 +0530

    Data Validation = Constants,Config and Artifact update

---
+++ networksecurity/components/data_validation.py
@@ -0,0 +1,9 @@
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
+from networksecurity.entity.artifact_entity import DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from scipy.stats import ks_2samp # Data Drift Report
+import pandas as pd
+import os,sys
+
